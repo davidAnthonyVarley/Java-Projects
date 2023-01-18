c88a7fdba8923f653d21f36a5683c555fa064a5c
bobAnthonyVarley <116462213+bobAnthonyVarley@users.noreply.github.com>
2023-01-18 22:23:57 +0000

    Update CreatingAGridWith2Darrays.java

--- CreatingAGridWith2Darrays.java
+++ CreatingAGridWith2Darrays.java
@@ -10,9 +10,9 @@ public class CreatingAGridWith2Darrays {
 		for (int column = 0; column<grid[row].length; column++) {
 			
 			if (row==0 || row==(grid.length-1))
-				grid[row][column] = BORDER_SPACE;
+				grid[row][column] = BORDER_SPACE; //if it's the first or last column, I want an outline. Adds sides to grid
 			else if (column==0 || column==(grid[row].length-1))
-				grid[row][column] = BORDER_SPACE;
+				grid[row][column] = BORDER_SPACE; //Adds top and bottom to grid
 			else
 				grid[row][column] = EMPTY_SPACE;
 		
@@ -29,8 +29,8 @@ public class CreatingAGridWith2Darrays {
 			for (int row = 0; row<grid.length; row++) {
 				
 				if (row==(grid.length-1))
-					System.out.println(grid[row][column]);
-				else
+					System.out.println(grid[row][column]); //prints grid[0][0] - grid[1][0] - grid[2][0]...
+				else                                           //   ... grid[0][1] - grid[1][1] - grid[2][1]...
 					System.out.print(grid[row][column]);
 			}
 		}
